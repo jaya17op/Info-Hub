49f7ff63c2b944db4d41d97ea44baeb8a4745ef0
giridhar <giridharpp07@gmail.com>
2024-07-06 00:41:28 +0530

    removed font

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -5,13 +5,13 @@ export const metadata={
     title:'InfoWeb',
     description:'InfoWeb is a web application helps you to connect and react ively with people around the world.',
 }
-const inter=Inter({subsets:["latin"]})
+// const inter=Inter({subsets:["latin"]})
 export default function RootLayout({
     children
 }:{children:React.ReactNode}){return(
     <ClerkProvider>
         <html lang="en">
-            <body className={`${inter.className} bg-dark-1`}>
+            <body className={` bg-dark-1`}>
                 {children}
             </body>
         </html>
