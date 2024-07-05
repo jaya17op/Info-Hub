6cfeddac5c7e9a5456facbc0ab49801da223756a
giridhar <giridharpp07@gmail.com>
2024-07-06 00:51:44 +0530

    fixed font

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,11 +1,9 @@
 import {ClerkProvider} from "@clerk/nextjs"
-import { Inter } from "next/font/google"
 import '../globals.css';
 export const metadata={
     title:'InfoWeb',
     description:'InfoWeb is a web application helps you to connect and react ively with people around the world.',
 }
-// const inter=Inter({subsets:["latin"]})
 export default function RootLayout({
     children
 }:{children:React.ReactNode}){return(
