ee057d7bccd71ca1f956123a5d80342f36fc927e
giridhar <giridharpp07@gmail.com>
2024-07-06 09:26:56 +0530

    added Suggested users component with working logic , search bar , pagination

--- components/shared/RightSidebar.tsx
+++ components/shared/RightSidebar.tsx
@@ -1,16 +1,45 @@
 
 
-function RightSidebar ()  {
+import UserCard from "../cards/UserCard";
+import { fetchUsers } from "@/lib/actions/user.actions";
+import { currentUser } from "@clerk/nextjs/server";
+
+async function RightSidebar() {
+  const user = await currentUser();
+  if (!user) return null;
+
+  const similarMinds = await fetchUsers({
+    userId: user.id,
+    pageSize: 4,
+  });
+
+
   return (
-   <section className="custom-scrollbar rightsidebar">
-      <div className="flex flex-1 flex-col justify-start text-light-1">
-        <h3 className="tex-heading4">Suggested Communities</h3>
+    <section className='custom-scrollbar rightsidebar'>
+
+      <div className='flex flex-1 flex-col justify-start'>
+        <h3 className='text-heading4-medium text-light-1'>Suggested Users</h3>
+        <div className='mt-7 flex w-[350px] flex-col gap-10'>
+          {similarMinds.users.length > 0 ? (
+            <>
+              {similarMinds.users.map((person) => (
+                <UserCard
+                  key={person.id}
+                  id={person.id}
+                  name={person.name}
+                  username={person.username}
+                  imgUrl={person.image}
+                  personType='User'
+                />
+              ))}
+            </>
+          ) : (
+            <p className='!text-base-regular text-light-3'>No users yet</p>
+          )}
+        </div>
       </div>
-      <div className="flex flex-1 flex-col justify-start text-light-1">
-        <h3 className="tex-heading4">Suggested Users</h3>
-      </div>
-   </section>
-  )
+    </section>
+  );
 }
 
-export default RightSidebar
+export default RightSidebar;
